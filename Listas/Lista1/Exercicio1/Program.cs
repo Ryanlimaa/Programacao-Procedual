6f5fa4391ac0b7dd80ba5cc1ef2e7a02506875fe
Ryanlimaa <ryan.limaa@outlook.com>
2024-02-19 21:36:39 -0300

    So testando com um exercicio feito em sala

---
+++ Listas/Lista1/Exercicio1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite a quantidade de dias que deseja converter!");
+
+        int DiaInicial = int.Parse(Console.ReadLine());
+
+        int ano = DiaInicial / 365;
+        int dia = DiaInicial % 365;
+
+        Console.WriteLine("O total deu " + ano + " ano(s) e, " + dia + " dia(s)");
+    }
+}
