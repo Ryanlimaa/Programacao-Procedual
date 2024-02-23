2dae694ccc61046b6e11ea8faee8e0859b8f30e2
Ryanlimaa <ryan.limaa@outlook.com>
2024-02-22 21:21:03 -0300

    lista 1 - Entrada e saida, operadores aritmeticos e operadores relacionados

--- Listas/Lista1/Exercicio2/obj/Debug/net8.0/Exercicio2.AssemblyInfo.cs
+++ Listas/Lista1/Exercicio2/obj/Debug/net8.0/Exercicio2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f5fa4391ac0b7dd80ba5cc1ef2e7a02506875fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d1413ecefb6b3d12d42c663e89b7f3e5a42a98d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercicio2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercicio2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
