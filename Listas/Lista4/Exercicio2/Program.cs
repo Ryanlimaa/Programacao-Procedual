1304b34acae1029c154946fd4c8c0b2d72cd9352
Ryanlima <ryan.limaa@outlook.com>
2024-03-13 22:04:58 -0300

    Lista 4 - Laços de repetição aninhados

---
+++ Listas/Lista4/Exercicio2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void  Main(string[] args)
+    {
+        const int NUMERO_MINIMO = 1;
+        const int NUMERO_MAXIMO = 9;
+
+        System.Console.WriteLine("Digite um numero:");
+        int tamanhoQuadrado = int.Parse(Console.ReadLine());
+
+        if (tamanhoQuadrado < NUMERO_MINIMO || tamanhoQuadrado > NUMERO_MAXIMO)
+        {
+            System.Console.WriteLine("altura invalida, insira uma altura entre 1 e 9");
+        }
+        else
+        {
+            for (int linha = 0; linha <= tamanhoQuadrado; linha++)
+            {
+                for (int coluna = 1; coluna <= linha; coluna++)
+                {
+                    System.Console.Write(coluna);
+                }
+                System.Console.WriteLine();
+            }
+        }
+    }
+}
