054cb0d1299602eeacbce18b141bfbcf9cc4b9c6
Ryanlima <ryan.limaa@outlook.com>
2024-04-11 19:23:06 -0300

    Lista 8 - Structs

---
+++ Listas/Lista8/Exercicio2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+struct Produto
+{
+    public int id;
+    public string nome;
+    public double preco;
+    public bool disponivelEmEstoque;
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int TAMANHO_VETOR = 3;
+        Produto[] dados = new Produto[TAMANHO_VETOR];
+
+        for (int i = 0; i < dados.Length; i++)
+        {
+            dados[i].id = int.Parse(Console.ReadLine());
+            dados[i].nome = Console.ReadLine();
+            dados[i].preco = double.Parse(Console.ReadLine());
+            dados[i].disponivelEmEstoque = bool.Parse(Console.ReadLine());
+        }
+
+        for (int i = 0; i < dados.Length; i++)
+        {
+            if (dados[i].disponivelEmEstoque)
+            {
+                System.Console.WriteLine(dados[i].nome);
+                System.Console.WriteLine(dados[i].preco);
+            }
+        }
+    }
+}
